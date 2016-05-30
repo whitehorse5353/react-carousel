c66b5b09d294517ff53eb5976e37702a99edc909
Priya Bhatt <pbha13@sapient.com>
2016-05-30 10:18:03 +0530

    Updated readme doc and app.js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # react-carousel
+
+Commands to run
+
+1) npm install
+2) run node app.js in one terminal window
+3) webpack-de-server in other window
--- app.js
+++ app.js
@@ -2,4 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('src'));
-app.listen(9000);
+app.listen(9000, function () {
+  console.log('listening on port 9000!');
+});
